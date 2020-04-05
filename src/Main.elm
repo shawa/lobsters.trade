c93e113c140c8215b7b02d88a0c215fd24d46148
Andrew Shaw <shawa1@tcd.ie>
2020-04-05 17:35:53 +0100

    Add a chart and some history
    Add line-charts to let us plot pricing history over time

--- src/Main.elm
+++ src/Main.elm
@@ -15,6 +15,8 @@ import Html
         , tr
         )
 import Html.Events exposing (onClick)
+import LineChart
+import List
 import Result
 import Translations.Account
 
@@ -32,10 +34,14 @@ main =
 type Msg
     = Buy
     | Sell
+    | Tick
 
 
 type alias Model =
-    Account
+    { account : Account
+    , time : Int
+    , price : Int
+    }
 
 
 subscriptions : Model -> Sub Msg
@@ -50,27 +56,43 @@ addCmd cmd model =
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    Account.empty
-        |> Account.setBalance 10
-        |> addCmd Cmd.none
+    ( { time = 0
+      , price = 0
+      , account = Account.empty |> Account.setBalance 10
+      }
+    , Cmd.none
+    )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        Tick ->
+            ( { model | time = model.time + 1 }
+            , Cmd.none
+            )
+
         Buy ->
-            model
-                |> Account.changeBalance -1
-                |> Result.andThen (Account.changeLobsters 1)
-                |> Result.withDefault model
-                |> addCmd Cmd.none
+            ( { model
+                | account =
+                    model.account
+                        |> Account.changeBalance -1
+                        |> Result.andThen (Account.changeLobsters 1)
+                        |> Result.withDefault model.account
+              }
+            , Cmd.none
+            )
 
         Sell ->
-            model
-                |> Account.changeBalance 1
-                |> Result.andThen (Account.changeLobsters -1)
-                |> Result.withDefault model
-                |> addCmd Cmd.none
+            ( { model
+                | account =
+                    model.account
+                        |> Account.changeBalance 1
+                        |> Result.andThen (Account.changeLobsters -1)
+                        |> Result.withDefault model.account
+              }
+            , Cmd.none
+            )
 
 
 view : Model -> Browser.Document Msg
@@ -80,8 +102,8 @@ view model =
     }
 
 
-viewAccount : Model -> Html Msg
-viewAccount model =
+viewAccount : Account -> Html Msg
+viewAccount account =
     let
         cell x =
             td [] [ text x ]
@@ -91,8 +113,8 @@ viewAccount model =
     in
     div []
         [ table []
-            [ viewField "Balance" (Translations.Account.balance model)
-            , viewField "Lobsters" (Translations.Account.lobsters model)
+            [ viewField "Balance" (Translations.Account.balance account)
+            , viewField "Lobsters" (Translations.Account.lobsters account)
             ]
         ]
 
@@ -106,10 +128,35 @@ viewControls =
         ]
 
 
+type alias ChartData =
+    { time : Int
+    , price : Int
+    }
+
+
+viewChart : Model -> Html Msg
+viewChart _ =
+    let
+        initData : List ChartData
+        initData =
+            List.range 0 100
+                |> List.indexedMap
+                    (\time price ->
+                        { time = time
+                        , price = time * 2
+                        }
+                    )
+    in
+    LineChart.view1 (.price >> toFloat)
+        (.time >> toFloat)
+        initData
+
+
 body : Model -> List (Html Msg)
 body model =
     [ h1 [] [ text "Open Lobster Exchange" ]
     , p [] [ text "Use your wit to get loads of money" ]
-    , viewAccount model
+    , viewAccount model.account
     , viewControls
+    , viewChart model
     ]
